88ef236f49ae00abef20dd83b4a6ccff2bef9ad3
Zhang Chi <chizhang@iis.ee.ethz.ch>
2026-03-24 14:48:01 +0100

    [INIT] velocity project

---
+++ init.sh
@@ -0,0 +1,19 @@
+conda activate py312
+source check_and_set_env.sh
+git submodule update --init --recursive
+
+if [  -n "${ZSH_VERSION:-}" ]; then
+    DIR="$(readlink -f -- "${(%):-%x}")"
+    SDK_HOME=$(dirname $DIR)
+else
+    SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+fi
+
+if [ -n "${GVSOC_WORKDIR}" ]; then
+    SDK_INSTALL=${GVSOC_WORKDIR}
+else
+    SDK_INSTALL=${SDK_HOME}
+fi
+
+export PATH=$SDK_INSTALL/install/bin:$PATH
+export PYTHONPATH=$SDK_INSTALL/install/python:$PYTHONPATH
